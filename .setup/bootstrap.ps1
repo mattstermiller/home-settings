070fe7d1d2a437786e26eea20f8aa0d73d6a7239
Matt Miller <miller.mattster@gmail.com>
2021-05-25 10:18:03 -0500

    bootstrap.ps1: Adds instructions

--- .setup/bootstrap.ps1
+++ .setup/bootstrap.ps1
@@ -1,4 +1,12 @@
-# run in user directory
+# 1. save this file to your user directory
+# 2. open powershell as admin
+# 3. cd to your user directory
+# 4. if you didn't already, allow running ps scripts: set-executionpolicy remotesigned
+# 5. run this script: ./bootstrap.ps1
+# 6. delete this script
+# 7. cd to .setup
+# 8. review and edit install.ps1 to install apps as desired
+# 9. run ./install.ps1
 
 # install chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
