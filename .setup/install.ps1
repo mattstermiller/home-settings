59e8fe80c47701db6b0336bc87ff5ac8f0429b97
Matt Miller <miller.mattster@gmail.com>
2024-08-21 06:57:05 -0500

    setup: adds regedit to disable start menu search, file organization

--- .setup/install.ps1
+++ .setup/install.ps1
@@ -3,13 +3,11 @@ choco install firefox
 choco install vim
 choco install microsoft-windows-terminal
 choco install arsclip
-#choco install katmouse
 choco install autohotkey
 choco install nircmd
 choco install vlc
 choco install irfanview
 choco install 7zip
-#choco install microsoft-teams
 #choco install slack
 #choco install gimp
 choco install lightshot
@@ -32,16 +30,16 @@ choco install dotnet-sdk
 #choco install smtp4dev
 #choco install postman
 
-# install plugins for apps
+# vim setup
 iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
     ni $HOME/vimfiles/autoload/plug.vim -Force
 vim +PlugInstall
 mkdir ..\vimfiles\swap
 
 # copy nuget.config since SDK does not create it
-copy .\NuGet.Config ..\AppData\Roaming\NuGet\
+copy .\resources\NuGet.Config ..\AppData\Roaming\NuGet\
 
-# add pins for apps that aggressively auto-update
+# add pins for apps that auto-update
 choco pin add -n=firefox
 choco pin add -n=foxitreader
 choco pin add -n=gitkraken
@@ -62,4 +60,7 @@ nircmd shortcut "$env:userprofile\.ahk\hotkeys.ahk" '~$folder.startup$' "Hotkeys
 $startMenu = "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs"
 copy "$startMenu\ArsClip.lnk" "$startMenu\Startup"
 
-REGEDIT /S KillCapsLock-ScrollLockToCapsLock.reg
+REGEDIT /S .\resources\disable-caps-lock_map-scroll-lock-to-caps-lock.reg
+REGEDIT /S .\resources\disable-web-search-in-start-menu-and-explorer.reg
+REGEDIT /S .\resources\disable-login-blur.reg
+
