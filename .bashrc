e5569ad6da876d0455c9380beef29b2291074da3
Matt Miller <miller.mattster@gmail.com>
2024-12-09 10:47:49 -0600

    bash/ps: Removes k8s env aliases

--- .bashrc
+++ .bashrc
@@ -20,11 +20,6 @@ alias gitdeploy="gitco develop && git push && gitco master && gitm origin/master
 alias windir='sed s,^/c,c:,g | sed s,/,\\\\,g'
 alias explore='/c/windows/explorer `echo $PWD | windir`'
 
-alias k8s-nphi-dev="az account set --subscription d5bb4703-0f15-481f-ab1c-9e5672c63b1f;az aks get-credentials --resource-group USC-RD-Canopy-RG --name aks-canopy-dev"
-alias k8s-dev="az account set --subscription ab875ab8-87c3-4db5-b508-61706a0467dc;az aks get-credentials --resource-group aks-canopy-dev --name aks-pri-canopy-dev"
-alias k8s-uat="az account set --subscription bf6ec698-9ef6-48fc-9f84-38a618677f6d;az aks get-credentials --resource-group aks-canopy-uat --name aks-pri-canopy-uat"
-alias k8s-prod="az account set --subscription 71745393-240e-4551-8a05-8a66edddbfe7;az aks get-credentials --resource-group aks-canopy-prod --name aks-pri-canopy-prod"
-
 function cd {
     if [[ ".lnk" = "${1:(-4)}" && -f "$1" ]] ;
         then builtin cd "$( follow "$1" )" ;
@@ -35,4 +30,3 @@ function cd {
 function base64d() {
     echo "$1" | base64 -d
 }
-
--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,3 @@
-function k8s-nphi-dev { az account set --subscription d5bb4703-0f15-481f-ab1c-9e5672c63b1f; az aks get-credentials --resource-group USC-RD-Canopy-RG --name aks-canopy-dev }
-function k8s-dev { az account set --subscription ab875ab8-87c3-4db5-b508-61706a0467dc; az aks get-credentials --resource-group aks-canopy-dev --name aks-pri-canopy-dev }
-function k8s-uat { az account set --subscription bf6ec698-9ef6-48fc-9f84-38a618677f6d; az aks get-credentials --resource-group aks-canopy-uat --name aks-pri-canopy-uat }
-function k8s-prod { az account set --subscription 71745393-240e-4551-8a05-8a66edddbfe7; az aks get-credentials --resource-group aks-canopy-prod --name aks-pri-canopy-prod }
-
-function k9s-nphi-dev { k9s --context aks-canopy-dev }
-function k9s-dev { k9s --context aks-pri-canopy-dev }
-function k9s-uat { k9s --context aks-pri-canopy-uat }
-function k9s-prod { k9s --context aks-pri-canopy-prod }
-
 # Import the Chocolatey Profile that contains the necessary code to enable
 # tab-completions to function for `choco`.
 # Be aware that if you are missing these lines from your profile, tab completion
