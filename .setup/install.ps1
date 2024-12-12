4e253be46ee82dc38c421b9fd8d22088bf63d775
Matt Miller <miller.mattster@gmail.com>
2024-12-12 16:27:44 -0600

    setup: Updates bootstrap instructions, fixes issues in install.ps1

--- .setup/install.ps1
+++ .setup/install.ps1
@@ -23,10 +23,11 @@ choco install visualstudio2019-workload-manageddesktopbuildtools
 # vim setup
 iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
     ni $HOME/vimfiles/autoload/plug.vim -Force
-vim +PlugInstall
+vim +PlugInstall +qall
 mkdir ..\vimfiles\swap
 
 # copy nuget.config since SDK does not create it
+mkdir ..\AppData\Roaming\NuGet\
 copy .\resources\NuGet.Config ..\AppData\Roaming\NuGet\
 
 # add pins for apps that auto-update
