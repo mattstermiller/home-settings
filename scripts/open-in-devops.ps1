21d73c8370c6f64f0a1ed0cf525a3b39cdd43937
Matt Miller <miller.mattster@gmail.com>
2026-02-06 14:35:06 -0600

    scripts: Fixes Open in Devops to properly handle dotfiles in root

--- scripts/open-in-devops.ps1
+++ scripts/open-in-devops.ps1
@@ -8,6 +8,6 @@ $RepoName = Split-Path -Leaf "$GitRoot"
 Push-Location $GitRoot
 $RelFilePath = Resolve-Path -Relative $FilePath
 Pop-Location
-$GitFilePath = $RelFilePath.TrimStart(".").Replace("\", "/")
+$GitFilePath = $RelFilePath.Replace("\", "/") -replace '^./', ''
 
 Start-Process "https://brightstarcare.visualstudio.com/Athena%20Business%20System/_git/${RepoName}?path=$GitFilePath"
